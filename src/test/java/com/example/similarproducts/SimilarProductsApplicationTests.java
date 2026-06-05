efcc9c13516b18b6ba49354b6cbbf5909b3df156
rcaglez <rafacala7@gmail.com>
2026-06-05 17:55:54 +0000

    feat: initial commit

---
+++ src/test/java/com/example/similarproducts/SimilarProductsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
